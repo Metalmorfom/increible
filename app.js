e00d06ea150aef479816d94862148989323f9385
Metalmorfom <cuentafolla@hotmail.com>
2023-10-23 07:55:32 -0300

    etapa con login, registro,noticias crud

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const connection = require('./database/db');
 
 //9 - establecemos las rutas
 app.get('/login', (req, res) => {
+
 	res.render('login');
 })
 
@@ -41,25 +42,25 @@ app.get('/login', (req, res) => {
 app.get('/register', async (req, res) => {
 	// Realiza una consulta para obtener las opciones de la tabla "junta_vecinal"
 	connection.query('SELECT id_junta, nombre FROM junta_vecinal', (error, results) => {
-	  if (error) {
-		console.log(error);
-	  } else {
-		// Realiza otra consulta para obtener las opciones de roles (como mencionamos anteriormente)
-		connection.query('SELECT id_rol, rol FROM rol', (error, rol) => {
-		  if (error) {
+		if (error) {
 			console.log(error);
-		  } else {
-			
-			res.render('register', { junta_vecinal: results, rol: rol });
-		  }
-		});
-	  }
+		} else {
+			// Realiza otra consulta para obtener las opciones de roles (como mencionamos anteriormente)
+			connection.query('SELECT id_rol, rol FROM rol', (error, rol) => {
+				if (error) {
+					console.log(error);
+				} else {
+
+					res.render('register', { junta_vecinal: results, rol: rol });
+				}
+			});
+		}
 	});
-  });
-  
+});
 
 
- 
+
+
 
 
 //10 - Método para la REGISTRACIÓN
@@ -82,49 +83,49 @@ app.post('/register', async (req, res) => {
 
 	let passwordHash = await bcrypt.hash(clave, 8);
 	try {
-	connection.query('INSERT INTO USER SET ?', {
-		rut: rut,
-		clave: passwordHash,
-		nombre: nombre,
-		s_nombre: s_nombre,
-		ap_paterno: paterno,
-		ap_materno: materno,
-		direccion: direccion,
-		correo: correo,
-		telefono: telefono,
-		profesion: profesion,
-		rol_id_rol: rol,
-		fecha_expiracion: fecha_expiracion,
-		fecha_registro:fecha_registro,
-		activa: state,
-		junta_vecinal_id_junta: junta_vecinal
-	} , async (error, results) => {
-		if (error) {
-			console.log(error);
-		} else {
-			res.render('register', {
-				alert: true,
-				alertTitle: "Registration",
-				alertMessage: "¡Successful Registration!",
-				alertIcon: 'success',
-				showConfirmButton: false,
-				timer: 1500,
-				ruta: '', junta_vecinal: results, rol: rol
-			});
-		 
-		}
-	});
+		connection.query('INSERT INTO USER SET ?', {
+			rut: rut,
+			clave: passwordHash,
+			nombre: nombre,
+			s_nombre: s_nombre,
+			ap_paterno: paterno,
+			ap_materno: materno,
+			direccion: direccion,
+			correo: correo,
+			telefono: telefono,
+			profesion: profesion,
+			rol_id_rol: rol,
+			fecha_expiracion: fecha_expiracion,
+			fecha_registro: fecha_registro,
+			activa: state,
+			junta_vecinal_id_junta: junta_vecinal
+		}, async (error, results) => {
+			if (error) {
+				console.log(error);
+			} else {
+				res.render('register', {
+					alert: true,
+					alertTitle: "Registration",
+					alertMessage: "¡Successful Registration!",
+					alertIcon: 'success',
+					showConfirmButton: false,
+					timer: 1500,
+					ruta: '', junta_vecinal: results, rol: rol
+				});
 
-} catch (error) {
-    console.log("Error al insertar en la base de datos:", error);
-    // Puedes mostrar un mensaje de error al usuario o realizar otra acción de manejo de errores aquí.
-  }
+			}
+		});
+
+	} catch (error) {
+		console.log("Error al insertar en la base de datos:", error);
+		// Puedes mostrar un mensaje de error al usuario o realizar otra acción de manejo de errores aquí.
+	}
 })
 
 process.on('uncaughtException', (err) => {
 	console.error('Error no capturado:', err);
 	// Puedes realizar un registro adicional o acciones de manejo de errores aquí.
-  });
+});
 
 //11 - Metodo para la autenticacion
 app.post('/auth', async (req, res) => {
@@ -144,12 +145,12 @@ app.post('/auth', async (req, res) => {
 					ruta: 'login'
 				});
 
-				
+
 				//res.send('Incorrect Username and/or Password!');				
 			} else {
 				//creamos una var de session y le asignamos true si INICIO SESSION       
 				req.session.loggedin = true;
-				req.session.name = results[0].name;
+				req.session.name = results[0].nombre;
 				res.render('login', {
 					alert: true,
 					alertTitle: "Conexión exitosa",
@@ -169,20 +170,37 @@ app.post('/auth', async (req, res) => {
 });
 
 //12 - Método para controlar que está auth en todas las páginas
+// Ruta para la página principal
 app.get('/', (req, res) => {
 	if (req.session.loggedin) {
-		res.render('index', {
+	  // El usuario está conectado, puedes continuar con la lógica para mostrar la página principal
+	  const userId = req.session.name;
+	  console.log('Valor de userId:', userId);
+	  connection.query('SELECT nombre FROM USER WHERE nombre = ?', [userId], (error, results) => {
+		if (error) {
+		  console.log(error);
+		  // Manejo de errores si es necesario
+		} else {
+		  const userName = results[0].nombre; // Suponiendo que el nombre del usuario se encuentra en la columna 'nombre'
+  
+		  res.render('index', {
+			userId: 'Bienvenido, ' + userId,
 			login: true,
-			name: req.session.name
-		});
+			name: 'Bienvenido, ' + userName,
+			results
+		  });
+		}
+	  });
 	} else {
-		res.render('index', {
-			login: false,
-			name: 'Debe iniciar sesión',
-		});
+	  // El usuario no está conectado, puedes redirigirlo a la página de inicio de sesión o mostrar un mensaje de error
+	  res.redirect('/login'); // Redirige a la página de inicio de sesión
+	  // O muestra un mensaje de error
+	  // res.render('error', { message: 'Debe iniciar sesión para acceder a la página principal' });
 	}
-	res.end();
-});
+  });
+  
+
+
 
 
 //función para limpiar la caché luego del logout
@@ -196,9 +214,139 @@ app.use(function (req, res, next) {
 //Destruye la sesión.
 app.get('/logout', function (req, res) {
 	req.session.destroy(() => {
-		res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
+		res.redirect('/login') // siempre se ejecutará después de que se destruya la sesión
 	})
 });
+
+
+
+
+
+
+
+// Ruta para mostrar el muro de noticias
+app.get('/noticias', (req, res) => {
+	const articleId = req.body.articleId;
+
+	if (req.session.loggedin) {
+	  // El usuario está conectado, puedes continuar con la lógica para mostrar noticias
+	  connection.query('SELECT n.id_muro, n.titulo, n.descripcion, n.fecha_public, CONCAT(u.nombre, " ", u.ap_paterno, " ", u.ap_materno) AS nombre_completo FROM noticia n JOIN USER u ON n.user_rut = u.rut ORDER BY n.fecha_public Desc', (error, noticias) => {
+		if (error) {
+		  console.error('Error al obtener noticias:', error);
+		  // Puedes manejar el error de alguna manera adecuada
+		}
+		
+		const userId = req.session.name;
+		console.log('Valor de userId en noticias:', userId);
+		connection.query('SELECT rut FROM USER WHERE nombre = ?', [userId], (error, results) => {
+		  if (error) {
+			console.log(error);
+			// Manejo de errores si es necesario
+		  } else {
+			const userName = results[0].nombre;
+  
+			res.render('noticias', {
+			  noticias,
+			  userId: 'Bienvenido, ' + userId,
+			  login: true,
+			  name: 'Bienvenido, ' + userName,
+			  results
+			});
+		  }
+		});
+	  });
+	} else {
+	  // El usuario no está conectado, puedes redirigirlo a la página de inicio de sesión o mostrar un mensaje de error
+	  res.redirect('/login'); // Redirige a la página de inicio de sesión
+	  // O muestra un mensaje de error
+	  // res.render('error', { message: 'Debe iniciar sesión para acceder a las noticias' });
+	}
+  });
+  
+
+
+
+
+// Ruta para crear una nueva noticia
+// Ruta para crear una nueva noticia
+app.post('/noticias', (req, res) => {
+	const { titulo, descripcion } = req.body;
+	const fecha_registro = new Date();
+
+	if (req.session.loggedin) {
+		const username = req.session.name;
+		connection.query('SELECT rut FROM USER WHERE nombre = ?', [username], (error, results) => {
+			if (error) {
+				console.log(error);
+				res.redirect('/login');
+				// Manejo de errores si es necesario
+			} else {
+				if (titulo && descripcion) {
+					const rut = results[0].rut; // Obtén el Rut del usuario
+					connection.query('INSERT INTO noticia (titulo, descripcion, fecha_public, user_rut) VALUES (?, ?, ?, ?)', [titulo, descripcion, fecha_registro, rut], (error, result) => {
+						if (error) {
+							console.error('Error al insertar la noticia:', error);
+							// Manejo de errores si es necesario
+						} else {
+							res.redirect('/noticias'); // Redirigir de nuevo al muro de noticias después de agregar la noticia
+						}
+					});
+				} else {
+					// Manejo de errores si los datos del formulario son incorrectos o faltan
+				}
+			}
+		});
+	} else {
+		res.redirect('/login'); // Redirigir al inicio de sesión si el usuario no está autenticado
+	}
+});
+
+
+
+
+
+
+app.post('/editar-noticia', (req, res) => {
+    // Obtén los datos enviados desde el formulario
+    const articleId = req.body.articleId;
+    const newTitle = req.body.titulo;
+    const newContent = req.body.descripcion;
+	
+	console.log('articleId '+articleId)
+	console.log('newTitle '+newTitle)
+	console.log('newContent '+newContent)
+
+    // Actualiza la noticia en la base de datos
+    connection.query('UPDATE noticia SET titulo = ?, descripcion = ? WHERE id_muro = ?', [newTitle, newContent, articleId], (error, result) => {
+        if (error) {
+            console.log(error);
+			console.log("no se editaron loco")
+            // Manejo de errores si es necesario
+        } else {
+            // Notificar el éxito de la actualización
+            res.redirect('/noticias');
+        }
+    });
+});
+
+
+
+
+// Ruta para actualizar una noticia
+app.put('/noticias/:id', (req, res) => {
+	// Lógica para actualizar una noticia en la base de datos
+});
+
+// Ruta para eliminar una noticia
+app.delete('/noticias/:id', (req, res) => {
+	// Lógica para eliminar una noticia de la base de datos
+});
+
+
+
+
+
+
 
 
 app.listen(3000, (req, res) => {
--- database/db.js
+++ database/db.js
@@ -1,24 +1,62 @@
-//2 - Invocamos a MySQL y realizamos la conexion
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    //Con variables de entorno
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE
-});
+const mysql = require('mysql2');
 
-connection.connect((error)=>{
-    if (error) {
-      console.error('El error de conexión es: ' + error);
-      return;
-    }
-    console.log('¡Conectado a la Base de Datos!');
-    console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASS:', process.env.DB_PASS);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
+let connection; // Cambia la variable de conexión a "let" para poder reconectar
+const maxConnectionRetries = 3; // Número máximo de intentos de reconexión
+let connectionRetries = 0;
 
+// Función para conectar a la base de datos
+function connectDatabase() {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
   });
 
-  module.exports = connection;
+  // Conecta a la base de datos y maneja errores de conexión
+  connection.connect((error) => {
+    if (error) {
+      console.error('Error de conexión a la base de datos:', error);
+      if (connectionRetries < maxConnectionRetries) {
+        // Intenta reconectar solo si no se ha alcanzado el límite de reintentos
+        console.log(`Intentando reconexión (#${connectionRetries + 1})...`);
+        connectionRetries++;
+        setTimeout(connectDatabase, 2000); // Espera 2 segundos antes de intentar reconectar
+      } else {
+        console.error('Se alcanzó el límite de reintentos de conexión.');
+        // Puedes implementar otras acciones de manejo de errores aquí.
+      }
+    } else {
+      console.log('¡Conectado a la Base de Datos!');
+      console.log('DB_HOST:', process.env.DB_HOST);
+      console.log('DB_USER:', process.env.DB_USER);
+      console.log('DB_PASS:', process.env.DB_PASS);
+      console.log('DB_DATABASE:', process.env.DB_DATABASE);
+      connectionRetries = 0; // Restablece los reintentos después de una conexión exitosa
+    }
+  });
+
+  // Manejo de errores de la conexión a la base de datos
+  connection.on('error', (err) => {
+    console.error('Error de conexión a la base de datos:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      // Reconectar
+      if (connectionRetries < maxConnectionRetries) {
+        console.log('Intentando reconectar a la base de datos (#' + connectionRetries + 1 + ')...');
+        connectionRetries++;
+        setTimeout(connectDatabase, 2000); // Espera 2 segundos antes de intentar reconectar
+      } else {
+        console.error('Se alcanzó el límite de reintentos de conexión.');
+        // Puedes implementar otras acciones de manejo de errores aquí.
+      }
+    } else {
+      // Otros tipos de errores que pueden requerir un manejo específico
+      console.error('Intento de reconexión fallido. Error:', err);
+    }
+  });
+}
+
+// Inicialmente, intenta conectar a la base de datos
+connectDatabase();
+
+module.exports = connection;
